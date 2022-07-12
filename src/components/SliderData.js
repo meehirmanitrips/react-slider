82225ad28882a38e5af437d9897c8e9dd4cb5391
Meehir Mani Tripathy <meehirmanitrips@gmail.com>
2022-07-12 21:56:16 +0530

    Project Finished

---
+++ src/components/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+  {
+    logo: require("./images/dollar.jpg"),
+  },
+  {
+    logo: require("./images/fun-3d-illustration-referee-with-vr-helmet_183364-81229.jpg"),
+  },
+  {
+    logo: require("./images/img2.jpg"),
+  },
+  {
+    logo: require("./images/men scoo.jpg"),
+  },
+  {
+    logo: require("./images/men shero.jpg"),
+  },
+  {
+    logo: require("./images/men workplace.jpg"),
+  },
+  {
+    logo: require("./images/v.jpg"),
+  },
+  {
+    logo: require("./images/vr.jpg"),
+  },
+];
